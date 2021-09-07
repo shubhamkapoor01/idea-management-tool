6f781e1e9693cde2243a52e7a7dd1d5f1f9cb729
Shubham Kapoor <shubham.kapoor01@gmail.com>
2021-09-07 17:48:29 +0530

    commit groupbytitle

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -15,6 +15,23 @@ function Body() {
 	const [isSorted, setIsSorted] = useState(false);
 	const [newContent, setNewContent] = useState("");
 	const [ideasList, setIdeasList] = useState(ideas);
+	const [ideasGroups, setIdeasGroups] = useState([]);
+
+	const enableDragDrop = () => { 
+		setIsSorted(false);
+	}
+
+	const SortButton = () => {
+		return isSorted ? (
+			<button className="features__sort" onClick={ enableDragDrop }>
+				Normal View
+			</button>
+		) : (
+			<button className="features__sort" onClick={ sortIdeas }>
+				Group By Title 
+			</button>
+		)
+	};
 
 	React.useEffect(() => { 
 		const fetch = async () => {
@@ -35,6 +52,7 @@ function Body() {
 		}
 		setTitle("");
 		setContent("");
+		setIsSorted(false);
 	}
 
 	const editIdea = (requiredId) => {
@@ -57,6 +75,7 @@ function Body() {
 		setIdeas(newIdeas);
 		setNewTitle("");
 		setNewContent("");
+		setIsSorted(false);
 	}
 
 	const deleteIdea = (requiredId) => {
@@ -71,21 +90,36 @@ function Body() {
 			}
 		}
 		setIdeas(newIdeas);
+		setIsSorted(false);
 	}
 
 	const sortIdeas = () => { 
-		if (isSorted) {
-			setIsSorted(false);
-		} else {
-			setIsSorted(true);
-		}
+		
 		console.log('sorting')
+		console.log(ideasGroups);
+
 		var newIdeas = [];
 		for (var i = 0; i < ideas.length; i ++) {
 			newIdeas.push(ideas[i]);
 		}
 		newIdeas.sort((a, b) => (a.title > b.title) ? 1 : -1);
 		setIdeas(newIdeas);
+
+		let idx = 0;
+		let newGroup = [];
+		for (var i = 0; i < newIdeas.length; i ++) {
+			newGroup[idx] = [];
+			newGroup[idx].push(newIdeas[i]);
+			while (i < newIdeas.length - 1 && newIdeas[i].title === newIdeas[i + 1].title) {
+				newGroup[idx].push(newIdeas[i + 1]);
+				i ++;
+			}
+			idx ++;
+		}
+
+		setIdeasGroups(newGroup);
+		console.log(ideasGroups);
+		setIsSorted(true);
 	}
 
 	const handleOnDragEnd = (result) => {
@@ -170,9 +204,7 @@ function Body() {
 							</button>
 						</div>
 					</Popup>
-					<button className="features__sort" onClick={ sortIdeas }>
-						Sort
-					</button>
+					<SortButton />
 					<button className="features__deleteall" onClick={ deleteAllIdeas }>
 						Delete All
 					</button>
@@ -181,52 +213,60 @@ function Body() {
 			
 			<div className="condition">
 				{ 
-					false ? (
-						ideas.map((idea) => {
-							return (
-								<div className="body__idea" key={ idea.id }>
-									<p className="body__idea__title">
-										{ idea.title }
-									</p>
-									<p className="body__idea__content">
-										{ idea.content }
-									</p>
-									<div className="body__idea__buttons">
-										<Popup trigger={
-											<button className="body__idea__edit">
-												Edit
-											</button>
-										} position="right top">
-											<div className="body__popup">
-												<input className="body__popup__titleinput" 
-													type="text" 
-													placeholder="Write the idea title here..."
-													value={ newTitle }
-													onChange={ (e) => setNewTitle(e.target.value) }
-												/>
-												<input className="body__popup__textinput" 
-													type="text" 
-													placeholder="Write the idea content here..."
-													value={ newContent }
-													onChange={ (e) => setNewContent(e.target.value) }
-												/>
-												<button className="body__popup__submit" type="submit" onClick={ () => { editIdea(idea.id) } }>
-													Done
-												</button>
-											</div>
-										</Popup>
-										<button className="body__idea__delete" onClick={ () => { deleteIdea(idea.id) } }>
-											Delete
-										</button>
+					isSorted ? (
+						<div className="body__ideas">
+							{ ideasGroups.map((ideasGroup) => {
+								return (
+									<div className="body__ideaGroup">
+										{ ideasGroup.map((idea) => {
+											return (
+												<div className="body__idea" key={ idea.id }>
+													<p className="body__idea__title">
+														{ idea.title }
+													</p>
+													<p className="body__idea__content">
+														{ idea.content }
+													</p>
+													<div className="body__idea__buttons">
+														<Popup trigger={
+															<button className="body__idea__edit">
+																Edit
+															</button>
+														} position="right top">
+															<div className="body__popup">
+																<input className="body__popup__titleinput" 
+																	type="text" 
+																	placeholder="Write the idea title here..."
+																	value={ newTitle }
+																	onChange={ (e) => setNewTitle(e.target.value) }
+																/>
+																<input className="body__popup__textinput" 
+																	type="text" 
+																	placeholder="Write the idea content here..."
+																	value={ newContent }
+																	onChange={ (e) => setNewContent(e.target.value) }
+																/>
+																<button className="body__popup__submit" type="submit" onClick={ () => { editIdea(idea.id) } }>
+																	Done
+																</button>
+															</div>
+														</Popup>
+														<button className="body__idea__delete" onClick={ () => { deleteIdea(idea.id) } }>
+															Delete
+														</button>
+													</div>
+												</div>	
+											)
+										})}
 									</div>
-								</div>	
-							)
-						})
+								)
+							})}
+						</div>
 				 	) : (
 					<DragDropContext className="body" onDragEnd={ handleOnDragEnd }>
 						<Droppable droppableId="ideas">
 							{(provided) => (
-								<ul {...provided.droppableProps} ref={provided.innerRef}>
+								<ul className="body__dragdrop" {...provided.droppableProps} ref={provided.innerRef}>
 									{ ideas.map((idea, index) => { 
 										return (
 											<Draggable draggableId={ idea.id }  key={ idea.id } index={ index }>
